658b53f7bcf6dec18a381137d7caa6e5c8e6a984
Mosbah Mohamed <mosbah@moltaqa.net>
2025-11-03 11:30:21 +0200

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/naqla_dashboard" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dashboard/naqla_dashboard/" : "/",
 
   pwa: {
     name: "Naqla admin Dashboard",
