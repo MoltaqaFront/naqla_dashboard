409b58372586cac1709d9efd9f89bc1bb76d7e6b
Mosbah Mohamed <mosbah@moltaqa.net>
2025-11-03 11:43:11 +0200

    fix upload

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/dashboard/naqla_dashboard" : "/",
+    process.env.NODE_ENV === "production" ? "/dashboard/naqla_dashboard/" : "/",
 
   pwa: {
     name: "Naqla admin Dashboard",
